43fa9cdb6c57947cfeab501ab98ba5919720015f
cyrilcon <constantine.sonic@gmail.com>
2025-05-24 18:04:28 +0500

    Add API versioning structure

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import router from "./routes/index.js";
+import v1Routes from "./api/v1/routes/index.js";
 
 const app = express();
 
 app.use(morgan("combined"));
 app.use(express.json());
-app.use(router);
+app.use("/api/v1", v1Routes);
 app.use(cors());
 
 export default app;
